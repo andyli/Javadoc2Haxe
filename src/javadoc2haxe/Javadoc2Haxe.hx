3bf729dab46d5643d3ccc204e3c4dc3ba9b61c3b
Andy Li <andy@onthewings.net>
2012-04-19 00:29:06 +0800

    dig out full name for everything

--- src/javadoc2haxe/Javadoc2Haxe.hx
+++ src/javadoc2haxe/Javadoc2Haxe.hx
@@ -225,6 +225,16 @@ class Javadoc2Haxe {
 		
 		var classFrame = new JQuery("body");
 		
+		var rTitlePack = ~/^(interface|class) in /;
+		new JQuery("a[title]").each(function(i, e){
+			var j = new JQuery(e);
+			if (rTitlePack.match(j.attr("title")) && j.children().length == 0) {
+				var pack = rTitlePack.replace(j.attr("title"), "");
+				if (j.text().indexOf(pack) == -1)
+					j.text(pack + "." + j.text());
+			}
+		});
+		
 		var classH2 = classFrame.find("h2:first");
 		var classPack = JQuery._static.trim(classH2.find("font").text());
 		var c = extractClassType(JQuery._static.trim(classH2.text().replace(classPack, "")));
@@ -256,16 +266,7 @@ class Javadoc2Haxe {
 		}
 		superclass = superclass == null || superclass == "java.lang.Object" ? null: mapType(superclass);
 		
-		var interfacesDd = classFrame.find("dl:contains('nterfaces'):first dd");
-		interfacesDd.find("a").each(function(i, e){
-			var j = new JQuery(e);
-			if (j.attr("title").length > 0) {
-				var pack = ~/.*\s+in\s+/.replace(j.attr("title"), "");
-				if (j.text().indexOf(pack) == -1)
-					j.text(pack + "." + j.text());
-			}
-		});
-		
+		var interfacesDd = classFrame.find("dl:contains('nterfaces'):first dd");		
 		var interfaceStr:String = interfacesDd.text();
 		var interfaces:List<String> = interfaceStr.split(",")
 			.filter(function(c) return c.length > 0)
